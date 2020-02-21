baed83ab20586f847c5fd5fb4514bae266f70ec1
Ashley Wang <ashleyswang@umail.ucsb.edu>
2020-02-20 19:13:55 -0800

    fix typos & add linux OS functionality

--- firewall.py
+++ firewall.py
@@ -4,7 +4,7 @@ import textwrap
 
 # Get ethernet frame from packet
 def ethernet_frame(data):
-	dest_mac, src_mac, proto = stuct.unpack('! 6s 6s H', data[:14])
+	dest_mac, src_mac, proto = struct.unpack('! 6s 6s H', data[:14])
 	return mac_addr(dest_mac), mac_addr(src_mac), socket.htons(proto), data[:14]
 
 # Get readable mac address (AA:BB:CC:DD:EE:FF)
@@ -14,14 +14,14 @@ def mac_addr(byte_addr):
 
 def main():
 	# For LINUX OS
-	# conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
+	conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
 	# For MAC OS
-	conn = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.ntohs(3))
+	# conn = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.ntohs(3))
 
 	while(1):
 		raw_data, addr = conn.recvfrom(65536)
-		dest_mac, src, ether_proto, data = ethernet_frame(raw_data)
-		print('\n Ethernet Frame: \nDestination: {} \tSource: {} \tProtocol: {}'\
+		dest_mac, src_mac, ether_proto, data = ethernet_frame(raw_data)
+		print('\nEthernet Frame: \nDestination: {} \tSource: {} \tProtocol: {}'\
 			.format(dest_mac, src_mac, ether_proto))
 
 main()
