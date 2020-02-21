712edea22fba3e23c2d1d8cf98fc910179e332c7
Ashley Wang <38049558+ashleyswang@users.noreply.github.com>
2020-02-20 19:05:19 -0800

    Add files via upload

---
+++ firewall.py
@@ -0,0 +1,27 @@
+import socket
+import struct
+import textwrap
+
+# Get ethernet frame from packet
+def ethernet_frame(data):
+	dest_mac, src_mac, proto = stuct.unpack('! 6s 6s H', data[:14])
+	return mac_addr(dest_mac), mac_addr(src_mac), socket.htons(proto), data[:14]
+
+# Get readable mac address (AA:BB:CC:DD:EE:FF)
+def mac_addr(byte_addr):
+	byte_str = map('{:02x}'.format, byte_addr)
+	return ':'.join(byte_str).upper()
+
+def main():
+	# For LINUX OS
+	# conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
+	# For MAC OS
+	conn = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.ntohs(3))
+
+	while(1):
+		raw_data, addr = conn.recvfrom(65536)
+		dest_mac, src, ether_proto, data = ethernet_frame(raw_data)
+		print('\n Ethernet Frame: \nDestination: {} \tSource: {} \tProtocol: {}'\
+			.format(dest_mac, src_mac, ether_proto))
+
+main()
