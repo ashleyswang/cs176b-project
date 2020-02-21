06507c6d3e62a6c5223f32f3c05d0d4b1117d7ad
Ashley Wang <ashleyswang@cs.ucsb.edu>
2020-02-20 20:02:31 -0800

    fix indentations

--- firewall.py
+++ firewall.py
@@ -4,49 +4,49 @@ import textwrap
 
 # Unpack ethernet frame
 def ethernet_frame(data):
-	dest_mac, src_mac, proto = struct.unpack('! 6s 6s H', data[:14])
-	return mac_addr(dest_mac), mac_addr(src_mac), socket.htons(proto), data[:14]
+    dest_mac, src_mac, proto = struct.unpack('! 6s 6s H', data[:14])
+    return mac_addr(dest_mac), mac_addr(src_mac), socket.htons(proto), data[:14]
 
 # Get readable mac address (AA:BB:CC:DD:EE:FF)
 def mac_addr(byte_addr):
-	byte_str = map('{:02x}'.format, byte_addr)
-	return ':'.join(byte_str).upper()
+    byte_str = map('{:02x}'.format, byte_addr)
+    return ':'.join(byte_str).upper()
 
 # Unpack IPv4 frame
 def ipv4_packet(data):
-	version_header_len = data[0]
-	version = version_header_len >> 4
-	header_length = (version_header_len & 15) * 4
-	ttl, proto, src, dest = struct.unpack('! 8x B B 2x 4s 4s', data[:20])
-	return version, header_length, ttl, proto, ipv4(src), ipv4(dest), data[header_length:]
+    version_header_len = data[0]
+    version = version_header_len >> 4
+    header_length = (version_header_len & 15) * 4
+    ttl, proto, src, dest = struct.unpack('! 8x B B 2x 4s 4s', data[:20])
+    return version, header_length, ttl, proto, ipv4(src), ipv4(dest), data[header_length:]
 
 # Get readable IPv4 address (X.X.X.X)
 def ipv4(addr):
-	return '.'.join(map(str, addr))
+    return '.'.join(map(str, addr))
 
 # Unpack ICMP packet
 def icmp_packet(data):
-	icmp_type, code, checksum = struct.unpack('! B B H', data[:4])
-	return icmp_type, code, checksum, data[4:]
+    icmp_type, code, checksum = struct.unpack('! B B H', data[:4])
+    return icmp_type, code, checksum, data[4:]
 
 # Unpack TCP segment
 def tcp_seg(data):
-	src_port, dest_port, seq, ack, offset_reserved_flags = \
-		struct.unpack('! H H L L H', data[:14])
-	offset = (offset_reserved_flags >> 12) * 4
-	flag_urg = (offset_reserved_flags & 32) >> 5
-	flag_ack = (offset_reserved_flags & 16) >> 4
-	flag_psh = (offset_reserved_flags & 8) >> 3
-	flag_rst = (offset_reserved_flags & 4) >> 2
-	flag_syn = (offset_reserved_flags & 2) >> 1
-	flag_fin = offset_reserved_flags & 1
-	return src_port, dest_port, seq, ack, flag_urg, flag_ack, flag_psh, flag_rst,\
-		flag_syn, flag_fin, data[offset:]
+    src_port, dest_port, seq, ack, offset_reserved_flags = \
+        struct.unpack('! H H L L H', data[:14])
+    offset = (offset_reserved_flags >> 12) * 4
+    flag_urg = (offset_reserved_flags & 32) >> 5
+    flag_ack = (offset_reserved_flags & 16) >> 4
+    flag_psh = (offset_reserved_flags & 8) >> 3
+    flag_rst = (offset_reserved_flags & 4) >> 2
+    flag_syn = (offset_reserved_flags & 2) >> 1
+    flag_fin = offset_reserved_flags & 1
+    return src_port, dest_port, seq, ack, flag_urg, flag_ack, flag_psh, flag_rst,\
+        flag_syn, flag_fin, data[offset:]
 
 # Unpack UDP segment
 def udp_seg(data):
-	src_port, dest_port, size = struct.unpack('! H H 2x H', data[:8])
-	return src_port, dest_port, size, data[8:]
+    src_port, dest_port, size = struct.unpack('! H H 2x H', data[:8])
+    return src_port, dest_port, size, data[8:]
 
 # String constants for formatting
 TAB_1 = '\t - '
@@ -69,19 +69,19 @@ def format_output_line(prefix, string, size=80):
             return '\n'.join([prefix + line for line in textwrap.wrap(string, size)])
 
 def main():
-	# For LINUX OS
-	conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
-	# For MAC OS
-	# conn = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.ntohs(3))
+    # For LINUX OS
+    conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
+    # For MAC OS
+    # conn = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.ntohs(3))
 
-	while(1):
-		raw_data, addr = conn.recvfrom(65536)
-		dest_mac, src_mac, ether_proto, data = ethernet_frame(raw_data)
-		print('\nEthernet Frame: \n\tDestination: {} \tSource: {} \tProtocol: {}'\
-			.format(dest_mac, src_mac, ether_proto))
+    while(1):
+        raw_data, addr = conn.recvfrom(65536)
+        dest_mac, src_mac, ether_proto, data = ethernet_frame(raw_data)
+        print('\nEthernet Frame: \n\tDestination: {} \tSource: {} \tProtocol: {}'\
+            .format(dest_mac, src_mac, ether_proto))
 
-		# IPv4
-		if eth_proto == 8:
+        # IPv4
+        if eth_proto == 8:
             (version, header_length, ttl, proto, src, target, data) = ipv4_Packet(data)
             print(TAB_1 + "IPV4 Packet:")
             print(TAB_2 + 'Version: {}, Header Length: {}, TTL: {}'.format(version, header_length, ttl))
